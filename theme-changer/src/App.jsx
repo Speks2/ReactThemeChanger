625b47a455edaac787a9465ed3a5f1a3e2367394
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-09-10 13:08:07 +0200

    ready

---
+++ theme-changer/src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './components/Theme/Theme.module.scss';
+
+function App() {
+  
+ conts [useChange, useClick] () 
+  return (
+    <>
+      <h1>Theme color changer button</h1>
+    </>
+  )
+}
+
+export default App
